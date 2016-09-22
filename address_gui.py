fc785a46ebe11766da9c3a1212090a1ae15806aa
Mr. Coxall <mr.coxall@mths.ca>
2016-09-21 20:24:43 -0400

    qwerty
    Initial commit

---
+++ address_gui.py
@@ -0,0 +1,9 @@
+# Created by: Anna Devlin
+# Created on: Sept 2016
+# Created for: ICS3U
+# This program is a Hello, World! program with a Gui
+
+import ui
+
+view = ui.load_view()
+view.present('sheet')
